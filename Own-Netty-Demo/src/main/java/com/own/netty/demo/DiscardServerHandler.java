c62e3b4f3c2dfaebcc08f214e0208dc22eb1abc9
roylic <lykismekk@outlook.com>
2023-01-10 23:52:27 +0800

    simple Discard Server under Netty's user guide

--- Own-Netty-Demo/src/main/java/com/own/netty/demo/DiscardServerHandler.java
+++ Own-Netty-Demo/src/main/java/com/own/netty/demo/DiscardServerHandler.java
@@ -1,4 +1,32 @@
 package com.own.netty.demo;
 
-public class DiscardServerHandler {
+import io.netty.buffer.ByteBuf;
+import io.netty.channel.ChannelHandlerContext;
+import io.netty.channel.ChannelInboundHandlerAdapter;
+
+public class DiscardServerHandler extends ChannelInboundHandlerAdapter {
+
+    @Override
+    public void channelRead(ChannelHandlerContext ctx, Object msg) {
+        // discard the received data
+        // byteBuf is a referenced-counted obj, which has to be released explicitly via release()
+        // it is the handler's responsibility to release any reference-counted obj passed to the handler
+        // usually channelRead is like:
+        // @Override
+        // public void channelRead(ChannelHandlerContext ctx, Object msg) {
+        //    try {
+        //        // Do something with msg
+        //    } finally {
+        //        ReferenceCountUtil.release(msg);
+        //    }
+        //}
+        ((ByteBuf) msg).release();
+    }
+
+    @Override
+    public void exceptionCaught(ChannelHandlerContext ctx, Throwable cause) throws Exception {
+        // close the connection when a exception is raised
+        cause.printStackTrace();
+        ctx.close();
+    }
 }
