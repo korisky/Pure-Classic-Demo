25b8e358bd794977551283a1f4c3ca36617c73fc
roylic <roylic@wokoworks.com>
2023-01-13 15:32:04 +0800

    adding spring application for testing

---
+++ Own-Spring-Demo/src/main/java/com/own/spring/demo/SpringDemoApplication.java
@@ -0,0 +1,15 @@
+package com.own.spring.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author Roylic
+ * 2023/1/13
+ */
+@SpringBootApplication
+public class SpringDemoApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(SpringDemoApplication.class, args);
+    }
+}
