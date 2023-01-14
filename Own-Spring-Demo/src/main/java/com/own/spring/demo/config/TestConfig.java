3fed1a5b935dfd06dda16a17adb31d897e793408
roylic <lykismekk@outlook.com>
2023-01-14 16:52:36 +0800

    correct the config

--- Own-Spring-Demo/src/main/java/com/own/spring/demo/config/TestConfig.java
+++ Own-Spring-Demo/src/main/java/com/own/spring/demo/config/TestConfig.java
@@ -5,11 +5,18 @@ import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.context.annotation.Configuration;
 
+import javax.annotation.PostConstruct;
+
 @Slf4j
 @Data
 @Configuration
-@ConfigurationProperties(prefix = "test.config")
+@ConfigurationProperties(prefix = "test")
 public class TestConfig {
 
     private String configName;
+
+    @PostConstruct
+    public void postConstruct() {
+        System.out.println();
+    }
 }
