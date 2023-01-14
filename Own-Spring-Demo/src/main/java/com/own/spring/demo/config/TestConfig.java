ebdb8f2610efed5974a6040335c90ff3b524a968
roylic <lykismekk@outlook.com>
2023-01-14 14:19:51 +0800

    adding configuration for with-args constructor

---
+++ Own-Spring-Demo/src/main/java/com/own/spring/demo/config/TestConfig.java
@@ -0,0 +1,15 @@
+package com.own.spring.demo.config;
+
+import lombok.Data;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.context.annotation.Configuration;
+
+@Slf4j
+@Data
+@Configuration
+@ConfigurationProperties(prefix = "test.config")
+public class TestConfig {
+
+    private String configName;
+}
