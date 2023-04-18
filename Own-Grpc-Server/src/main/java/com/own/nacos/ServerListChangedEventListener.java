3ed022404222d05928f8dcea810cc50fe9ca2f33
roylic <lykismekk@outlook.com>
2023-04-18 23:01:52 +0800

    still not working for Nacos event subscription

--- Own-Grpc-Server/src/main/java/com/own/nacos/ServerListChangedEventListener.java
+++ Own-Grpc-Server/src/main/java/com/own/nacos/ServerListChangedEventListener.java
@@ -1,12 +1,10 @@
 package com.own.nacos;
 
 import com.alibaba.fastjson2.JSON;
-import com.alibaba.fastjson2.JSONObject;
-import com.alibaba.nacos.client.naming.event.InstancesChangeEvent;
+import com.alibaba.nacos.client.naming.event.ServerListChangedEvent;
 import com.alibaba.nacos.common.notify.Event;
 import com.alibaba.nacos.common.notify.NotifyCenter;
 import com.alibaba.nacos.common.notify.listener.Subscriber;
-import io.grpc.stub.AbstractStub;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Component;
 
@@ -14,16 +12,16 @@ import javax.annotation.PostConstruct;
 
 @Slf4j
 @Component
-public class NewInstanceEventListener extends Subscriber<InstancesChangeEvent> {
+public class ServerListChangedEventListener extends Subscriber<ServerListChangedEvent> {
 
     @Override
     public Class<? extends Event> subscribeType() {
-        return InstancesChangeEvent.class;
+        return ServerListChangedEvent.class;
     }
 
     @Override
-    public void onEvent(InstancesChangeEvent instancesChangeEvent) {
-        log.info(">>> Received Nacos InstancesChangeEvent: {}", JSON.toJSONString(instancesChangeEvent));
+    public void onEvent(ServerListChangedEvent serverListChangedEvent) {
+        log.info(">>> Received Nacos ServerListChangedEvent: {}", JSON.toJSONString(serverListChangedEvent));
         System.out.println();
     }
 
