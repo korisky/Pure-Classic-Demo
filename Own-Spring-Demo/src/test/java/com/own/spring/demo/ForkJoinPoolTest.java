ce79ffd52b605dde92b8184e09d0bf67972e3021
roylic <lykismekk@outlook.com>
2023-09-12 23:03:30 +0800

    tmp cmt

---
+++ Own-Spring-Demo/src/test/java/com/own/spring/demo/ForkJoinPoolTest.java
@@ -0,0 +1,15 @@
+package com.own.spring.demo;
+
+import org.junit.Test;
+
+import java.util.concurrent.ForkJoinPool;
+
+public class ForkJoinPoolTest {
+
+    @Test
+    public void forkJoinPoolStuff() {
+
+        ForkJoinPool forkJoinPool = ForkJoinPool.commonPool();
+
+    }
+}
