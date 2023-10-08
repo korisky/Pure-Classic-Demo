b0922e57cd9600019452f0c1e8a3bed2cb7a6d26
roylic <roylic@wokoworks.com>
2023-10-08 11:36:22 +0800

    still not working, could not compile SpringBoot 2.x to GraalVM Native Image stuff

--- own-graal-migration-demo/src/main/java/com/own/controller/HelloController.java
+++ own-graal-migration-demo/src/main/java/com/own/controller/HelloController.java
@@ -1,6 +1,5 @@
 package com.own.controller;
 
-import org.springframework.nativex.hint.AotProxyHint;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -13,7 +12,6 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/hello")
 public class HelloController {
 
-    @AotProxyHint
     @GetMapping("/graal")
     public String hello_graal() {
         return "Hello, graalvm";
